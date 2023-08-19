74fbdcc43658f3c23660c7cdabfd8ab7305b162b
John <2003l.jm@gmail.com>
2023-08-19 11:59:46 +0300

    header works

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
     path('home.html', views.home, name='home'),
     path('contact.html', views.contact, name='contact'),
