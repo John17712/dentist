60103c5a2eb7a364b8e29055a030e37ea847eaa7
John17712 <142434544+John17712@users.noreply.github.com>
2023-08-22 12:11:29 +0300

    Update views.py

--- website/views.py
+++ website/views.py
@@ -36,9 +36,6 @@ def contact(request):
 def about(request):
 	return render(request, 'about.html', {})
 
-def home(request):
-	return render(request, 'home.html', {})
-
 
 def service(request):
 	return render(request, 'service.html', {})
