a7ae62549f64fe68ad79ca97cc09d3d9aa83f1fd
John17712 <142434544+John17712@users.noreply.github.com>
2023-08-22 12:10:58 +0300

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
-    path('home.html', views.home, name='home'),
     path('contact.html', views.contact, name='contact'),
     path('about.html', views.about, name='about'),
     path('service.html', views.service, name='service'),
